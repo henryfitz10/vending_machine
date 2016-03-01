4da5d961aabc874d78a6802a75dbc85abd5da387
Henry FitzGerald <hfitzgerald@seabreeze.ie>
2016-03-01 15:42:17 +0000

    Initial commit

---
+++ test_file.py
@@ -0,0 +1,15 @@
+import unittest
+
+from vending_machine import give_change
+
+class TestVendingMachine(unittest.TestCase):
+    def test_this_thing_is_on(self):
+        self.assertNotEquals(1, 1)
+
+def give_change(self, amount):
+    return [10, 5, 2]
+
+
+
+
+
---
+++ vending_machine.py
@@ -0,0 +1,22 @@
+def give_change(amount):
+coins = [100, 50, 20, 10, 5, 2, 1]
+    change = []
+    for coin in coins:
+        if coin <= amount:
+            amount -= coin
+            change.append(coin)
+    return change
+
+print give_change(645)
+
+
+
+
+
+
+
+
+
+
+
+
